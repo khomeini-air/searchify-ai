4690de268fff261223bd1e59bd8bc9df9dafee51
Ahmed Hagras <ahmedabdelsattar96@gmail.com>
2022-09-20 17:52:44 +0200

    update1

--- recommendation_api.py
+++ recommendation_api.py
@@ -10,13 +10,17 @@ class suggestions(BaseModel):
     tags: list[constr(max_length=255)]
 
 
+#Build post function that will recive the data(domain,tags) and return the suggestions result
 @app.post('/')
 async def recommendatio(item:suggestions):
+    #authorization to connect with neo4j graph database
     graph = Graph('neo4j+s://ed887860.databases.neo4j.io:7687', auth=('neo4j','yulO-mLLrt72cJ39FI12Lo-Lr6wtv6qx2oIzUNDl5Zo'))
-    # item_dict = json.load(item)
+
+    #getting the domain and tags from item as it sent as json object
     domain = item.domain
     tags = item.tags
 
+    #build quary body that contain the recommendation algorithm(content based recommendation algorithm) by adding domain and tags
     quary = '''
     MATCH (domain:Domain)<-[:IS_LINKED_WITH]->(tag:Tag)<-[:IS_LINKED_WITH]->(sug:Suggestion)
     WHERE domain.name CONTAINS "'''+domain+'''"
@@ -25,21 +29,27 @@ async def recommendatio(item:suggestions):
     RETURN DISTINCT sug.name
     LIMIT 10
     '''
+    #add first tag with AND
     tag = tags[0]
     tag_qu = ' AND tag.name CONTAINS "'+ tag +'"'
     substr = "WITH sug"
     idx = quary.index(substr)
     quary = quary[:idx] + tag_qu + quary[idx:]
+
+    #adding other tags with OR 
     for tag in tags[1:]:
         tag_qu = ' OR tag.name CONTAINS "'+ tag+'"'
         substr = "WITH sug"
         idx = quary.index(substr)
         quary = quary[:idx] + tag_qu + quary[idx:]
 
+    #run the quary and getiing the results
     result = graph.run(quary)
     
+    #converting the tabled result to dataframe to extract the suggestions from it
     result_pd = pd.DataFrame(result.to_data_frame())
-    # print('this is the empty dataframe',result_pd)
+    
+    #make condition in case there is no suggestions not send error but just send there is no suggestions
     if result_pd.empty:
         result_fi = {'suggestion':'No suggestions avaliable'}
     else:
